1314372dfed31f4eebf85009435c7bfcb9fd596d
KuoJoo <joonas.kuokkanen@hotmail.com>
2025-04-09 13:39:15 +0300

    Delete albumApi directory

--- albumApi/scripts/seedTestDatabase.js
+++
@@ -1,9 +0,0 @@
-const albums = [
-  { title: 'Kesän lapsi', artist: 'Tapani kansa', year: 1976, genre: 'Iskelmä', tracks: 12, owner: '67ec5bd4713349698b67744e' },
-  { title: 'Joku', artist: 'Testi', year: 2001, genre: 'Iskelmä', tracks: 13, owner: '67ec5bd4713349698b67744e' },
-  { title: 'Imperiumin Vastaisku', artist: 'Eppu normaali', year: 1988, genre: 'Suomirock', tracks: 9, owner: '67ec6204babb4435897a8ff4' },
-  { title: 'Myrskyn jälkeen', artist: 'Kari Tapio', year: 1995, genre: 'Iskelmä', tracks: 12, owner: '67ec6204babb4435897a8ff4' },
-  { title: 'Toto IV', artist: 'Toto', year: 1982, genre: 'Rock', tracks: 10, owner: '67ec51d45aaeb826d62d9131' },
-];
-
-module.exports = albums;
